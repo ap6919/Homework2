d402570042cdf5dd7a5c83c7416e2420bebabb9c
ap6919 <ap6919@nyu.edu>
2021-09-21 14:00:50 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -15,7 +15,7 @@ def webServer(port=13331):
     connectionSocket, addr = serverSocket.accept()
     try:
       try:
-        message = connectionSocket.recv().decode()
+        message = connectionSocket.recv(2048).decode()
         filename = message.split()[1]
         f = open(filename[1:])
         outputdata = f.readlines()
